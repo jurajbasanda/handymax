c0c55d3d99a9388725adda6aa05411cf82585393
Juraj Basanda <basanda.juraj@gmail.com>
2021-01-19 01:43:59 +0100

    update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from 'react'
 
 export default function Footer() {
-    const style = {color:'black',
-                    }
-    const footerStyle = {display:'flex',
-                        flexDirection:'row',
-                        justifyContent: 'flex-end',
-                        alignItems:'center',
-                        height:'100%',
-                        maxWidth:'1200px',
-                        marginTop:'5vh'
-                    }
-    return (
-        <footer style={footerStyle}>
-           <a href="https://jurajbasanda.com/" style={style}> Made by  ©Juraj Basanda</a>
-        </footer>
-    )
+	const style = { color: 'black' }
+	const footerStyle = {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		height: '100%',
+		maxWidth: '1200px',
+		marginTop: '5vh',
+	}
+	return (
+		<footer style={footerStyle}>
+			<a href='https://jurajbasanda.com/' style={style}>
+				{' '}
+				Made by ©Juraj Basanda
+			</a>
+		</footer>
+	)
 }
--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -1,27 +1,45 @@
-import React, { Component } from 'react';
-import './home.css';
+import React from 'react';
+import './reviews.scss';
+import userImg from '../svg/user-regular.svg';
 
-export class Reviews extends Component {
-    
-    render() {
-const fb1 = "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdaisy.russell.758%2Fposts%2F10157794311401206&width=500";
-const fb2 ='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fkireshatayneah.lindo%2Fposts%2F10220879810844659&width=500';
-const fb3 ='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Flewis.espeut.9%2Fposts%2F376595476289678&width=500';
-const fb4 = 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fsharahi%2Fposts%2F10163540543010445&width=500'
+const Reviews=()=>  {
+const reviews = [
+                {name:'Lucie Regan',
+                disc:"I would highly recommend Handymax. Premek fitted a new garden fence for us and went above and beyond to get it finished in a day.  He has also moved lightswitches, an aerial socket and plugs and did a very clean and tidy job.  He's very polite and professional.",
+                link:'https://www.facebook.com/lucie.regan.9/posts/1767472763393116',
+                },
+                {name:'Marek Vancisin',
+                date:'21/08/2020',
+                link:'https://www.facebook.com/marek.vancisin/posts/10218761478412202',
+                disc:'Highly recommend. Job was done properly. All the way 5 star.'
+                  },
+                {disc:'Highly recommend ! Reliable and Excellent service!Wallpaper, tiles on floor and walls, plumbing amazing all rounder would defiantly use again!',
+                  name:'Krystle Purdy Krystalized',
+                  link:'https://www.facebook.com/krystle.day/posts/10157634532462060',
+                },
+                {
+                  disc:"Fantastic job on flooring, painting, doors - great service, lovely people, reliable and made you feel like they could cope with anything. Finishing is perfect, looks expensive but done for a great price. Thank you.",
+                  name:'Aryan Sharahi',
+                  link:'https://www.facebook.com/sharahi/posts/10163540543010445'
+                }]
 return (
             <section className='fb'>
-
-<h1>What Our Customers Say About Us</h1>
-
+            <h1>What Our Customers Say About Us</h1>
             <div className='reviewsList'>  
-          <iframe src={fb1} title="review" width="500" height="220"  scrolling="no" frameBorder="0"  allow="encrypted-media"></iframe>
-          <iframe src={fb2} title="review" width="500" height="220"  scrolling="no" frameBorder="0"  allow="encrypted-media"></iframe>
-          <iframe src={fb3} title="review" width="500" height="220" scrolling="no" frameBorder="0"  allow="encrypted-media"></iframe>
-          <iframe src={fb4} title="review" width="500" height="220"  scrolling="no" frameBorder="0"  allow="encrypted-media"></iframe>
+          {/*<iframe src={fb4} title="review" width="500" height="220"  scrolling="no" frameBorder="0"  allow="encrypted-media"></iframe>*/}
+          {reviews.map(review =>(
+                      <div className='review-item'>
+                      <a href={review.link} target='_blank' rel="noopener noreferrer">
+                      <img src={userImg} alt="User" srcSet=""/>                     
+                      <h3>{review.name}</h3>
+                    <p>{review.disc}</p>
+                      </a>
+                      </div>
+          ))}
           </div>
           </section>
         )
     }
-}
+
 
 export default Reviews
